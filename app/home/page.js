b2b787cc3cc11ec84a03d94c201e1c8f19bc402a
Nourhan <nourhanmahmed1@gmail.com>
2024-04-04 22:33:35 +0200

    update

---
+++ app/home/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function Home() {
+    return (<>
+
+        <Link href={'/about'}>About</Link>
+        <h1>home
+
+
+        </h1></>);
+}
+
+export default Home;
